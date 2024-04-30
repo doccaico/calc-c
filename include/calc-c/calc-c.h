cb5a8fbfa7d67af0ec6bcf8a9f9763c7df4e2ff9
doccaico <hassy.luna@gmail.com>
2024-04-30 15:05:07 +0900

    up

--- include/calc-c/calc-c.h
+++ include/calc-c/calc-c.h
@@ -1,2 +1,6 @@
+#define VERSION "0.0.1"
+
+char* version(void);
+
 int add(int a, int b);
 int sub(int a, int b);
--- src/calc-c.c
+++ src/calc-c.c
@@ -1,2 +1,5 @@
+#include "calc-c/calc-c.h"
+
+char* version() { VERSION; }
 int add(int a, int b) { return a + b; }
 int sub(int a, int b) { return a - b; }
