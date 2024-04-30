3f5685873f784f7f10e754538cbb39fbec74e0fd
doccaico <hassy.luna@gmail.com>
2024-04-30 15:40:00 +0900

    up

--- include/calc-c/calc-c.h
+++ include/calc-c/calc-c.h
@@ -1,4 +1,7 @@
 #define CALC_C_VERSION "0.0.1"
 
+#define ADD(x, y) ((x) + (y))
+#define SUB(x, y) ((x) - (y))
+
 int add(int a, int b);
 int sub(int a, int b);
--- src/calc-c.c
+++ src/calc-c.c
@@ -1,4 +1,4 @@
 #include "calc-c/calc-c.h"
 
-int add(int a, int b) { return a + b; }
-int sub(int a, int b) { return a - b; }
+int add(int a, int b) { return ADD(a, b); }
+int sub(int a, int b) { return SUB(a, b); }
